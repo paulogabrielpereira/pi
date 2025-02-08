f7e891b5ca3a5aa7311200812ed799e10df6e855
paulogabriel <paulogabrieldeoliveira2@gmail.com>
2025-02-07 23:04:20 -0300

    final commit

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ document.addEventListener("DOMContentLoaded", function (){
 
         const elementoSelecionado = event.target.closest(".element-data");
         const iframeSelecionado = elementoSelecionado.lastElementChild.firstElementChild;
-
         const iframeDoc = iframeSelecionado.contentDocument || iframeSelecionado.contentWindow.document;
         const script = iframeDoc.createElement("script");
         const params = new URLSearchParams();
@@ -27,12 +26,41 @@ document.addEventListener("DOMContentLoaded", function (){
         iframeDoc.body.appendChild(script);
 
         const linkIframe = iframeDoc.querySelector('.more-link');
-        console.log(linkIframe)
 
         linkIframe.addEventListener("click", function(event){
             const params = new URLSearchParams();
             params.set('numero', elementoSelecionado.querySelector('.number').textContent);
             event.target.setAttribute('href', `elements.html?${params.toString()}`);
         });
+
+        const rect = iframeSelecionado.getBoundingClientRect();
+        divIframe = iframeSelecionado.parentElement;
+        console.log(rect.y)
+
+        if (rect.y < 90) {
+            if (rect.x < 600) {
+                divIframe.style.left = '380%';
+                divIframe.style.bottom = '-190%';
+            } else {
+                divIframe.style.left = '-280%';
+                divIframe.style.bottom = '-190%';
+            }
+        } else if (rect.y < 250) {
+            if (rect.x < 600) {
+                divIframe.style.left = '396%';
+                divIframe.style.bottom = '-50%';
+            } else {
+                divIframe.style.left = '-296%';
+                divIframe.style.bottom = '-50%';
+            }
+        } else {
+            if (rect.x < 90) {
+                divIframe.style.left = '396%';
+                divIframe.style.bottom = '90%'
+            } else if (rect.x > 1100) {
+                divIframe.style.left = '-296%';
+                divIframe.style.bottom = '90%';   
+            }
+        } 
     });
 });
